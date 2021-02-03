3299b6fc16922e0b0473155b4f3dff50d72300da
AWS ParallelCluster user <ec2-user@ip-10-0-2-249.us-east-2.compute.internal>
2021-02-03 22:03:49 +0000

    fix: specify temp dir for deduplication

--- wrapper.sh
+++ wrapper.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 DATE=$(date +"%Y%m%d%H%M")
 
-snakemake -p --rerun-incomplete --cluster "qsub -S /bin/bash -V -j y -o logs -cwd -pe mpi {threads}" --jobs 60 --latency-wait 300 &> WGS_${DATE}.out"
+snakemake -p --use-conda --rerun-incomplete --cluster "qsub -S /bin/bash -V -j y -o logs/ -cwd -pe mpi {threads}" --jobs 60 --latency-wait 300 &> WGS_${DATE}.out
