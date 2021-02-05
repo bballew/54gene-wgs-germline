fd603e0669f1abd231ba632177bafeae4855b307
Bari J. Ballew <bari.ballew@gmail.com>
2021-02-05 15:40:22 +0000

    feat: use mamba for env resolution

--- wrapper.sh
+++ wrapper.sh
@@ -6,4 +6,11 @@ DATE=$(date +"%Y%m%d%H%M")
 
 mkdir -p logs/
 
-snakemake -p --use-conda --rerun-incomplete --cluster "qsub -S /bin/bash -V -j y -o logs/ -cwd -pe mpi {threads}" --jobs 60 --latency-wait 300 &> WGS_${DATE}.out
+snakemake -p \
+	--use-conda \
+	--conda-frontend mamba \
+	--rerun-incomplete \
+	--cluster "qsub -S /bin/bash -V -j y -o logs/ -cwd -pe mpi {threads}" \
+	--jobs 25 \
+	--latency-wait 300 \
+	&> WGS_${DATE}.out
