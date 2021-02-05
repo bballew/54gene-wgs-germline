5891dbf3153cff51f108b1374d7d9bff9c96730a
Bari J. Ballew <bari.ballew@gmail.com>
2021-02-05 04:52:06 +0000

    feat: create logs dir in wrapper

--- wrapper.sh
+++ wrapper.sh
@@ -4,4 +4,6 @@ set -euo pipefail
 
 DATE=$(date +"%Y%m%d%H%M")
 
+mkdir -p logs/
+
 snakemake -p --use-conda --rerun-incomplete --cluster "qsub -S /bin/bash -V -j y -o logs/ -cwd -pe mpi {threads}" --jobs 60 --latency-wait 300 &> WGS_${DATE}.out
