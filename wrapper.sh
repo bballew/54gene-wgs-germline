c726dd5e07eefd94852deea3b2c2071daccb9160
Bari J. Ballew <bari.ballew@gmail.com>
2021-02-08 05:10:17 +0000

    fix: got java to recognize new temp path for trimming by exporting global variables

--- wrapper.sh
+++ wrapper.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 
 DATE=$(date +"%Y%m%d%H%M")
 
+t=$(awk '($0~/^tempDir/){print $2}' config/config.yaml | sed "s/['\"]//g")
+mkdir -p "${t}"
+export TMPDIR="${t}"
+export JDK_JAVA_OPTIONS=-Djava.io.tmpdir="${t}"
+export _JAVA_OPTIONS=-Djava.io.tmpdir="${t}"
+export IBM_JAVA_OPTIONS=-Djava.io.tmpdir="${t}"
 mkdir -p logs/
 
 snakemake -p \
@@ -11,6 +17,6 @@ snakemake -p \
 	--conda-frontend mamba \
 	--rerun-incomplete \
 	--cluster "qsub -S /bin/bash -V -j y -o logs/ -cwd -pe mpi {threads}" \
-	--jobs 25 \
+	--jobs 40 \
 	--latency-wait 300 \
 	&> WGS_${DATE}.out
