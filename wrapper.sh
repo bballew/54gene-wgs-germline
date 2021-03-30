7a87de56d191d79c53dc77a472c664896fd2317d
Bari J. Ballew <bari.ballew@gmail.com>
2021-03-30 19:49:13 -0400

    refactor: move cluster submission string from wrapper.sh to user config

--- wrapper.sh
+++ wrapper.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 
 DATE=$(date +"%Y%m%d%H%M")
 
-f=$(awk '($0~/^sampleFile/){print $2}' config/config.yaml | sed "s/['\"]//g")
 j=$(awk '($0~/^jobs/){print $2}' config/config.yaml)
+cluster_mode=$(awk '($0~/^cluster_mode/){print $0}' config/config.yaml | sed "s/\"/'/g" | awk -F\' '($0~/^cluster_mode/){print $2}')
 
 snakemake -p \
 	--use-conda \
 	--conda-frontend mamba \
 	--rerun-incomplete \
 	--default-resources mem_mb=1024 batch=1 \
-	--cluster "qsub -S /bin/bash -V -j y -o logs/ -cwd -pe mpi {threads} -l h_vmem={resources.mem_mb}M" \
+	--cluster "${cluster_mode}" \
 	--resources batch=${j} \
 	--jobs ${j} \
 	--latency-wait 300 \
