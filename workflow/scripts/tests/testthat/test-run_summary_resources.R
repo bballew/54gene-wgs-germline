d7e52c1fed0607615c509fa95a5a5b4514877f24
Bari J. Ballew <bari.ballew@54gene.com>
2022-01-24 17:41:15 -0500

    feat: add sex discordant table to report
    re #40

--- workflow/scripts/tests/testthat/test-run_summary_resources.R
+++ workflow/scripts/tests/testthat/test-run_summary_resources.R
@@ -7,6 +7,7 @@ input.subjects <- c("A", "B", "C", "D", "E", "F", "G")
 output.subjects.filename <- "testthat_resources/output_subjects.tsv"
 exclude.reasons.filename <- "testthat_resources/exclude_reasons.tsv"
 somalier.relatedness.filename <- "testthat_resources/somalier.pairs.tsv"
+somalier.sex.filename <- "testthat_resources/somalier.samples.tsv"
 fastqc.filename <- "testthat_resources/multiqc_fastqc_1.txt"
 bcftools.stats.filename <- "testthat_resources/joint_called_stats.out"
 
@@ -64,17 +65,15 @@ test_that("add.fastqc.data returns lane and read for failing fastq files for spe
     input.df <- data.frame(Subject = input.subjects,
 			    "QC Outcome for This Run" = c("Pass", "low_depth", "Pass", "contamination,low_depth", "No", "Pass", "high_het_hom"),
 			    check.names = FALSE)
-    expected <- cbind(input.df, 
-			"Per Base Sequence Quality Failures" = rep("", nrow(input.df)), 
-			"Per Base N Content Failures" = rep("", nrow(input.df)), 
+    expected <- cbind(input.df,
+			"Per Base Sequence Quality Failures" = rep("", nrow(input.df)),
+			"Per Base N Content Failures" = rep("", nrow(input.df)),
 			"Overrepresented Sequences Failures" = rep("", nrow(input.df)),
 			"Rerun Recommendation" = c("Rerun subset of fastqs", "Fail", "Pass", "Fail", "Fail", "Pass", "Fail"))
     expected[1, 3] = "S1_L002_r1, S1_L002_r2"
     expected[1, 4] = "S1_L002_r1"
     expected[2, 5] = "S1_L001_r1"
 	observed <- add.fastqc.data(input.df, fastqc.filename)
-	print(expected)
-	print(observed)
     expect_identical(observed, expected)
 })
 
@@ -86,4 +85,10 @@ test_that("add.coverage returns dataframe with additional column enumerating cov
 	expected$Coverage <- c(18.5, 22.4, 23.6, 29.1, 28.7, 25.2, 19.1)
     observed <- add.coverage(input.df, bcftools.stats.filename)
 	expect_identical(observed, expected)
+})
+
+test_that("report.sex.discordances returns dataframe with subject, inferred, and self-reported sex, if discordant", {
+	observed <- report.sex.discordances(output.subjects.filename, somalier.sex.filename)
+	expected <- data.frame("Subject" = c("C", "F"), "Inferred Sex" = c(1, 2), "Self-reported Sex" = c("female", "male"), check.names = FALSE)
+	expect_equal(observed, expected)
 })
