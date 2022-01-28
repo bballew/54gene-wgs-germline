ebb0248ded1ccedc40cb69bd90188c8303cf016e
Esha Joshi <esha.joshi@54gene.com>
2022-01-28 19:32:31 +0000

    fix: modify fixtures, add test for encode_sex

--- workflow/scripts/tests/test_generate_ped.py
+++ workflow/scripts/tests/test_generate_ped.py
@@ -30,15 +30,16 @@ def ped_format():
         "d": "-9"
     })
 
-def encodedsex_format():
-    return pd.DataFrame({
-        "Sample": ["Sample1", "Sample2"],
-        "a": ["Sample1", "Sample2"],
-        "c": [0, 0],
-        "b": [0, 0],
-        "Sex": [1, 2],
-        "d": "-9"
-    })    
+""" Can't call multiple fixtures directly in an elegant way for 
+test_add_ped_columns so here I create a fixture that just calls 
+the ped_format() function to return the dataframe and use as 
+input for test_encoded_sex.
+
+Reference: https://github.com/pytest-dev/pytest/issues/3950 """
+
+@pytest.fixture(name="ped_format_indirect")
+def ped_format_indirect():
+    return ped_format() 
 
 # testing functions
 
@@ -49,8 +50,6 @@ def encodedsex_format():
 def test_check_if_ped(test_in, exp_out):
     test_out = generate_ped.check_if_ped(test_in)
     assert exp_out == test_out
-
-# read_in_linker
 
 # check_column_number
 def test_check_column_number(passing_linker):
@@ -68,12 +67,15 @@ def test_check_column_headers_fail(incorrect_headers):
     with pytest.raises(Exception):
         generate_ped.check_column_headers(incorrect_headers)
 
-# add_ped_columns and encoded sex
-def test_add_ped_columns_and_encode_sex(passing_linker):
-     ped_out = generate_ped.add_ped_columns(passing_linker)
-     exp_ped_out = ped_format() 
-     pd.testing.assert_frame_equal(ped_out, exp_ped_out)
+# add_ped_columns
+def test_add_ped_columns(passing_linker):
+    ped_out = generate_ped.add_ped_columns(passing_linker)
+    exp_ped_out = ped_format()
+    pd.testing.assert_frame_equal(ped_out, exp_ped_out)
 
-     encode_out = generate_ped.encode_sex(exp_ped_out)
-     exp_encode_out = encodedsex_format()
-     pd.testing.assert_frame_equal(encode_out, exp_encode_out)
+# encode_sex
+def test_encoded_sex(ped_format_indirect):
+    encoded_out = generate_ped.encode_sex(ped_format_indirect)
+    ped_format_example = ped_format()
+    exp_encode_out = ped_format_example.replace({'Sex': {'M':1, 'F':2}})
+    pd.testing.assert_frame_equal(encoded_out, exp_encode_out)
