27fe0f4af50abebfc545e7567e5fb099e08e4118
Lightning Auriga <lightning.auriga@gmail.com>
2023-12-15 10:36:16 -0500

    chore: update authorship

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = "54gene-wgs-germline"
-copyright = "2022, B. Ballew, E. Joshi, C. Palmer, 54gene"
-author = "Bari Jane Ballew, Esha Joshi, Cameron Palmer"
+copyright = "2022, B. Ballew, E. Joshi, L. Auriga, 54gene"
+author = "Bari Jane Ballew, Esha Joshi, Lightning Auriga"
 version = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
